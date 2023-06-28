b02a39d12dd29841811a231ae13568fdb5de8a40
leandroKachu <lesilvajunior@hotmail.com>
2023-06-28 11:27:19 -0700

    create route and page home

--- src/controllers/pages.go
+++ src/controllers/pages.go
@@ -12,3 +12,7 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 func LoadViewCreate(w http.ResponseWriter, r *http.Request) {
 	utils.RunTemplate(w, "formUser.html", nil)
 }
+
+func LoadHomePage(w http.ResponseWriter, r *http.Request) {
+	utils.RunTemplate(w, "home.html", nil)
+}
---
+++ src/router/rotas/home.go
@@ -0,0 +1,15 @@
+package rotas
+
+import (
+	"net/http"
+	"webapp/src/controllers"
+)
+
+var rotaHome = []Rota{
+	{
+		URI:        "/home",
+		Metodo:     http.MethodGet,
+		Funcao:     controllers.LoadHomePage,
+		RequerAuth: true,
+	},
+}
